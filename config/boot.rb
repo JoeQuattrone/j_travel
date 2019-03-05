0dcf1966e4505d4c0263201c13f00623d52f9a15
Joe Quattrone <joequattrone5@gmail.com>
2019-03-05 14:44:42 -0500

    Made navbar thinner

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'active_support/dependencies' #suggested line to add from this website from rafael franca
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
