75bb6ea108d9c415dba74539317119bed212f367
Joe Quattrone <joequattrone5@gmail.com>
2019-03-04 20:35:02 -0500

    require active support method added to boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'active_support/dependencies' #suggested line to add from this website from rafael franca
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
