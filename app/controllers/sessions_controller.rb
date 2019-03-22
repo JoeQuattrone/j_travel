441f083f8dcf19554ee741bc023aa910bc99ffb0
Joe Quattrone <joequattrone5@gmail.com>
2019-03-22 10:32:38 -0400

    Omniauth patch

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_or_create_by(facebook_id: auth['uid']) do |u|
+    @user = User.find_or_create_by(email: auth['info']['email']) do |u|
       u.first_name =  auth['info']['name'].split(" ").first
       u.last_name =  auth['info']['name'].split(" ").last
       u.email = auth['info']['email']
+      u.facebook_id = auth['uid']
 
-      u.password = SecureRandom.hex
+      if u.password.nil?
+        u.password = SecureRandom.hex
+      end
     end
     session[:user_id] = @user.id
 
